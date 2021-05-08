93ef887afe84f7e31fe741dedf02709a9b8b2fd2
GGolfz <wisarutgolf11@gmail.com>
2021-05-08 23:53:09 +0700

    start learning method :sparkles:

---
+++ 12-method/method.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type User struct {
+	Username      string
+	FullName      string
+	ProfilePicUrl string
+}
+
+func (u User) Info() {
+	fmt.Printf("User name: %v\n", u.Username)
+	fmt.Printf("Full name: %v\n", u.FullName)
+	fmt.Printf("Profile Picture URL: %v\n", u.ProfilePicUrl)
+}
+
+func main() {
+	u := User{
+		Username:      "golang",
+		FullName:      "Basic Golang",
+		ProfilePicUrl: "https://knowhere.fake/gopher.jpg",
+	}
+	u.Info()
+}
